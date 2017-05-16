76eced5e1da621cc462308353140f6af8b79679d
Setherizor <setherizor@gmail.com>
2017-05-16 12:16:59 -0400

    Fixed home button dependencies

---
+++ homeButton.js
@@ -0,0 +1,6 @@
+var home = document.createElement("a");
+home.innerHTML =
+    "<a href='../index.html'><img style='width: 60px' src='home.png'><a>";
+home.style = "position: absolute; top: 0; right: 5px;";
+
+document.body.appendChild(home);
